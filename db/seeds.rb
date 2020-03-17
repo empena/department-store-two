5b416f5d18ee90795307fdafb217e07cf7d7c201
Emily Pena <emilyapena@gmail.com>
2020-03-17 15:02:16 -0600

    Stable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shoes = Department.create(name:'Shoes', description:'Shoe Department')
+# 5.times do
+#   shoes.Items.create(
+#     name: Faker::Commerce.product_name,
+#     description: Faker::Lorem.sentence,
+#     price: Faker::Commerce.price.to_f,
+#   )
+# end
+
+mensClothing = Department.create(name:'Mens Clothing', description:"Find Men's Clothing here")
+
+# 5.times do
+#   mensClothing.Items.create(
+#     name: Faker::Commerce.product_name,
+#     description: Faker::Lorem.sentence,
+#     price: Faker::Commerce.price.to_f )
+# end
+
+
+womensClothing = Department.create(name:'Womens Clothing', description:"Find Women's Clothing here")
+
+# 5.times do
+#   womensClothing.Items.create(
+#     name: Faker::Commerce.product_name,
+#     description: Faker::Lorem.sentence,
+#     price: Faker::Commerce.price.to_f,
+#     department: Faker::Commerce.department
+#   )
+# end
+puts "Department/items Seeded"
